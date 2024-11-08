e8f5e443cf4bbdd1afbfbee758a1dec141b4fc3f
Dongchunhua1225 <hdong25@uw.edu>
2024-11-07 20:21:35 -0800

    searching whether this user has bind his phone number

--- power-business/member-service/src/main/java/com/powernode/controller/MemberController.java
+++ power-business/member-service/src/main/java/com/powernode/controller/MemberController.java
@@ -1,16 +1,16 @@
 package com.powernode.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.powernode.domain.Member;
 import com.powernode.model.Result;
 import com.powernode.service.MemberService;
+import com.powernode.util.AuthUtils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
 
 //微信小程序会员业务管理控制层
 @Api(tags = "微信小程序业务接口管理")
@@ -33,6 +33,18 @@ public class MemberController {
     public Result<String> modifyMemberInfo(@RequestBody Member member) {
         Boolean modified = memberService.modifyMemberInfoByOpenId(member);
         return Result.handle(modified);
+    }
+
+    @ApiOperation("查询会员是否绑定手机号码")
+    @GetMapping("isBindPhone")
+    public Result<Boolean> loadMemberIsBindPhone() {
+        //获取会员的openid
+        String openid = AuthUtils.getMemberOpenId();
+
+        //根据会员openid查询会员详情
+        Member member = memberService.getOne(new LambdaQueryWrapper<Member>().eq(Member::getOpenId, openid));
+
+        return Result.success(StringUtils.hasText(member.getUserMobile()));
     }
 
 }
